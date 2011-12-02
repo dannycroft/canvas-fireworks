bb092bc283041b75b7568e6f76a0d88c32e65019
Andy Skelton <skeltoac@gmail.com>
2011-12-02 16:28:50 -0600

    Add color config

--- js/canvas.js
+++ js/canvas.js
@@ -8,20 +8,37 @@
 */
 
 (function( $ ) {
+	var orange = "#FA5600",
+	    tan    = "#DBC170",
+	    red    = "#E01309",
+	    green  = "#A0AF28",
+	    moss   = "#003D04",
+	    sky    = "#0051A4",
+	    blue   = "#1010FF",
+	    yellow = "#FFFF00",
+	    white  = "#FFFFFF";
+
 	var defaults = {
-		renderInterval : 40, // ms between rendered frames
+		renderInterval : 50, // ms between rendered frames
 		stepInterval : 10, // ticks between timeline steps
 		drag : 0.01, // velocity lost per frame
 		gravity : 0.5, // downward acceleration
 		wind : -0.2, // horizontal slide applied to everything each frame
 		sprites : { // images used in animation
-			rocket   : "img/electric.png",
-			explosion: "img/spark.png",
-			core     : "img/spark.png",
-			shell    : "img/star.png",
-			ring     : "img/electric.png"
+			rocket    : "img/electric.png",
+			explosion : "img/spark.png",
+			core      : "img/spark.png",
+			shell     : "img/star.png",
+			ring      : "img/electric.png"
 		},
-		fov : 300,
+		spriteColors : {
+			rocket    : [blue],
+			explosion : [yellow, white],
+			core      : [orange, yellow, white],
+			shell     : [green, red, orange, tan, green, white],
+			ring      : [moss, sky],
+		},
+		fov : 400,
 		imgs : {}, // loaded sprites
 		loading : 0,
 		particles : [], // all particles ever created
@@ -31,15 +48,10 @@
 		timer : null,
 	};
 
-	var Fireworks = function(options) {
-		$.extend(true, this, defaults, options); // Allow opts to override the vars above
-		this.loadSprites(); // blocks animation while loading sprites
-	};
-
 	var methods = {
 		init : function( options ) {
 			return this.each(function(){
-				var $this = $(this);console.log($this);
+				var $this = $(this);
 				var fireworks = new Fireworks(options);
 				$this.data("fireworks", fireworks);
 				fireworks.initCanvas(this);
@@ -57,6 +69,11 @@
 				$(this).data("fireworks").stop();
 			});
 		}
+	};
+
+	function Fireworks(options) {
+		$.extend(true, this, defaults, options); // Allow opts to override the vars above
+		this.loadSprites(); // blocks animation while loading sprites
 	};
 
 	Fireworks.prototype.initCanvas = function(canvas) {
@@ -78,25 +95,168 @@
 		return this;
 	};
 
+	/**
+	 * Converts HSV to RGB value.
+	 *
+	 * @param {Integer} h Hue as a value between 0 - 360 degrees
+	 * @param {Integer} s Saturation as a value between 0 - 100 %
+	 * @param {Integer} v Value as a value between 0 - 100 %
+	 * @returns {Array} The RGB values  EG: [r,g,b], [255,255,255]
+	 */
+	function hsvToRgb(h,s,v) {
+
+		var s = s / 100,
+		v = v / 100;
+
+		var hi = Math.floor((h/60) % 6);
+		var f = (h / 60) - hi;
+		var p = v * (1 - s);
+		var q = v * (1 - f * s);
+		var t = v * (1 - (1 - f) * s);
+
+		var rgb = [];
+
+		switch (hi) {
+		case 0: rgb = [v,t,p];break;
+		case 1: rgb = [q,v,p];break;
+		case 2: rgb = [p,v,t];break;
+		case 3: rgb = [p,q,v];break;
+		case 4: rgb = [t,p,v];break;
+		case 5: rgb = [v,p,q];break;
+		}
+
+		var r = Math.min(255, Math.round(rgb[0]*256)),
+		g = Math.min(255, Math.round(rgb[1]*256)),
+		b = Math.min(255, Math.round(rgb[2]*256));
+
+		return [r,g,b];
+
+	}	
+
+	/**
+	 * Converts RGB to HSV value.
+	 *
+	 * @param {Integer} r Red value, 0-255
+	 * @param {Integer} g Green value, 0-255
+	 * @param {Integer} b Blue value, 0-255
+	 * @returns {Array} The HSV values EG: [h,s,v], [0-360 degrees, 0-100%, 0-100%]
+	 */
+	function rgbToHsv(r, g, b) {
+
+		var r = (r / 255),
+		g = (g / 255),
+  		b = (b / 255);	
+
+		var min = Math.min(Math.min(r, g), b),
+		max = Math.max(Math.max(r, g), b),
+		delta = max - min;
+
+		var value = max,
+		saturation,
+		hue;
+
+		// Hue
+		if (max == min) {
+			hue = 0;
+		} else if (max == r) {
+			hue = (60 * ((g-b) / (max-min))) % 360;
+		} else if (max == g) {
+			hue = 60 * ((b-r) / (max-min)) + 120;
+		} else if (max == b) {
+			hue = 60 * ((r-g) / (max-min)) + 240;
+		}
+
+		if (hue < 0) {
+			hue += 360;
+		}
+
+		// Saturation
+		if (max == 0) {
+			saturation = 0;
+		} else {
+			saturation = 1 - (min/max);
+		}
+
+		return [Math.round(hue), Math.round(saturation * 100), Math.round(value * 100)];
+	}
+
+	function hexToRgb(hex) {
+		hex = hex.replace(/^#/, '');
+		var r = parseInt("0x" + hex.substr(0,2));
+		var g = parseInt("0x" + hex.substr(2,2));
+		var b = parseInt("0x" + hex.substr(4,2));
+		return [r, g, b];
+	}
+
+	function hexToHsv(hex) {
+		return rgbToHsv.apply({}, hexToRgb(hex));
+	}
+
 	Fireworks.prototype.loadSprites = function() {
 		var self = this;
 		$.each(this.sprites, function(name, url) {
 			++self.loading;
 			var img = new Image();
 			img.src = url;
-			img.onload = function() { --self.loading; };
-			self.imgs[name] = img;
+			img.onload = function() {
+				if ( typeof self.imgs[name] != "object" )
+					self.imgs[name] = [];
+				self.varySprite(name, img);
+				--self.loading;
+			};
 		});
 	};
 
-	Fireworks.prototype.launchRocket = function(data) {
-		if ( !data )
-			return;
+	Fireworks.prototype.varySprite = function(name, img) {
+		var canvas = document.createElement('canvas'), c=canvas.getContext('2d');
+		canvas.width = img.width;
+		canvas.height = img.height;
+		c.drawImage(img, 0, 0);
+		var imageData = c.getImageData(0, 0, img.width, img.height);
+		var pix = imageData.data.length / 4;
+		var pixHsv = [];
+		for ( var p = 0; p < pix; ++p ) {
+			var i = p * 4;
+			pixHsv[p] = rgbToHsv(imageData.data[i], imageData.data[i+1], imageData.data[i+2]);
+		}
+		var colors = this.spriteColors[name];
+		for ( var color = 0; color < colors.length; ++color ) {
+			var colorHsv = hexToHsv(colors[color]);
+			for ( p = 0; p < pix; ++p ) {
+				var rgb = hsvToRgb(colorHsv[0], colorHsv[1] * pixHsv[p][1] / 100, pixHsv[p][2]);
+				i = p * 4;
+				imageData.data[i] = rgb[0];
+				imageData.data[i+1] = rgb[1];
+				imageData.data[i+2] = rgb[2];
+			}
+			canvas = this.imgs[name][color] = document.createElement('canvas');
+			canvas.width = img.width;
+			canvas.height = img.height;
+			c = canvas.getContext('2d');
+			c.putImageData(imageData, 0, 0);
+			this.imgs[name].push(canvas);
+			for ( p = 0; p < pix; ++p ) {
+				var rgb = hsvToRgb(colorHsv[0], (200 + colorHsv[1] * pixHsv[p][1] / 100) / 3, (200 + pixHsv[p][2]) / 3);
+				i = p * 4;
+				imageData.data[i] = rgb[0];
+				imageData.data[i+1] = rgb[1];
+				imageData.data[i+2] = rgb[2];
+			}
+			canvas = this.imgs[name][color] = document.createElement('canvas');
+			canvas.width = img.width;
+			canvas.height = img.height;
+			c = canvas.getContext('2d');
+			c.putImageData(imageData, 0, 0);
+			this.imgs[name].push(canvas);
+		}
+	}
+
+	Fireworks.prototype.launchRocket = function(idx) {
 		opts={
 			pos: new Vector3((Math.random() * 100) - 50, 190, 0),
-			vel: new Vector3((Math.random() * 10) - 5, (Math.random() * 3) - 18, (Math.random() * 6) - 3),
-			img: this.imgs.rocket,
-			data: data,
+			vel: new Vector3((Math.random() * 10) - 5, (Math.random() * 5) - 20, (Math.random() * 6) - 3),
+			img: this.imgs.rocket.random(),
+			data: this.timeline[ this.step ][ idx ],
 			cont: function(particle) {
 				// Continue while rising
 				if ( particle.vel.y < 0 )
@@ -106,12 +266,11 @@
 				particle.fireworks.explodeShell(particle.pos, particle.data[1]);
 				particle.fireworks.explodeRing(particle.pos, particle.data[2]);
 				// Become bright, expand, contract, fade away
-				particle.img = particle.fireworks.imgs.explosion;
+				particle.img = particle.fireworks.imgs.explosion.random();
 				particle.vel = new Vector3(0, 0, 0);
-				particle.grav = 0.1;
-				particle.drag = 0.8;
+				particle.grav = 0;
 				var x = Math.sqrt(particle.data[0]) / 10;
-				particle.scales = [1,2,3,4,5,5,4,4,3,3,2,2,0].map(function(s){return s*x;});
+				particle.scales = [1,2,4,5,5,5,4,4,4,3,3,2,2,0].map(function(s){return s*x;});
 				particle.cont = function(particle) { return particle.scale = particle.scales.shift(); };
 				return true;
 			}
@@ -122,9 +281,12 @@
 	Fireworks.prototype.explodeShell = function(pos, mag) {
 		if ( mag <= 0 )
 			return;
-		var root = Math.sqrt(mag);
+		var root = Math.sqrt(mag) + 1;
 		var vel = new Vector3(root, root, root);
 		var cont = function(p) { return --p.timer > 0 || Math.random() > 0.3; }
+		var imgs = [];
+		imgs[0] = this.imgs.shell.random();
+		imgs[1] = this.imgs.shell.random();
 		// Spawn a symmetrical pair of particles for each unit magnitude
 		for ( var i = 0; i < mag; ++i ) {
 			vel.rotate(Math.random(), Math.random(), Math.random());
@@ -132,19 +294,19 @@
 			this.getParticle({
 				pos: pos.copy(),
 				vel: myVel,
-				grav: 0.2,
+				grav: 0.4,
 				drag: 0.9,
 				cont: cont,
-				img: this.imgs.shell,
+				img: imgs.random(),
 				timer: 20,
 			});
 			this.getParticle({
 				pos: pos.copy(),
 				vel: myVel.invert(),
-				grav: 0.2,
+				grav: 0.4,
 				drag: 0.9,
 				cont: cont,
-				img: this.imgs.shell,
+				img: imgs.random(),
 				timer: 20,
 			});
 		}
@@ -153,31 +315,32 @@
 	Fireworks.prototype.explodeRing = function(pos, mag) {
 		if ( mag <= 0 )
 			return;
-		var root = Math.sqrt(mag);
+		var root = Math.sqrt(mag) + 1;
 		var vel = new Vector3(root, 0, root);
 		var cont = function(p) { return --p.timer > 0 || Math.random() > 0.3; }
 		var rX = 1 - 2 * Math.random();
 		var rZ = 1 - 2 * Math.random();
+		var img = this.imgs.ring.random();
 		for ( var i = 0; i < mag; ++i ) {
 			vel.rotateY(Math.random());
 			var myVel = vel.copy().rotateX(rX).rotateZ(rZ).multiplyEq( 1.5 + Math.random() / 5 );
 			this.getParticle({
 				pos: pos.copy(),
 				vel: myVel,
-				grav: 0.2,
-				drag: 0.9,
+				grav: 0.4,
+				drag: 0.92,
 				cont: cont,
-				img: this.imgs.ring,
+				img: img,
 				scale: 0.7,
 				timer: 20,
 			});
 			this.getParticle({
 				pos: pos.copy(),
 				vel: myVel.invert(),
-				grav: 0.2,
-				drag: 0.9,
+				grav: 0.4,
+				drag: 0.92,
 				cont: cont,
-				img: this.imgs.ring,
+				img: img,
 				scale: 0.7,
 				timer: 20,
 			});
@@ -190,7 +353,7 @@
 		var root = Math.sqrt(mag) / 3;
 		var vel = new Vector3(root, root, root);
 		var cont = function(p) { return --p.timer > 0 || Math.random() > 0.3; }
-		for ( var i = 0; i < mag; ++i ) {
+		for ( var i = 0; i < mag / 2; ++i ) {
 			vel.rotate(Math.random(), Math.random(), Math.random());
 			var myVel = vel.copy().multiplyEq( Math.random() / 2 + .25);
 			this.getParticle({
@@ -229,13 +392,19 @@
 		return particle;
 	};
 
-	Fireworks.prototype.draw3Din2D = function(particle) {
+	Fireworks.prototype.draw3Din2D = function(particle, idx) {
 		var scale = this.fov / ( this.fov + particle.pos.z );
 		var x2d = ( particle.pos.x * scale) + this.canvas.width / 2;
 		var y2d = ( particle.pos.y * scale) + this.canvas.height / 2;
 		scale *= 6 * particle.scale;
+		if ( typeof idx != "number" )
+			idx = 0;
+		if ( particle.img.length )
+			var img = particle.img[ idx % particle.img.length ];
+		else
+			var img = particle.img;
 		if (scale > 0)
-			this.context.drawImage(particle.img, x2d - scale, y2d - scale, scale * 2, scale * 2);
+			this.context.drawImage(img, x2d - scale, y2d - scale, scale * 2, scale * 2);
 	};
 
 	Fireworks.prototype.render = function() {
@@ -244,7 +413,7 @@
 		var i;
 		if ( this.tick % this.stepInterval == 0 ) {
 			for ( i = 0; i < this.timeline[this.step].length; ++i )
-				this.launchRocket(this.timeline[this.step][i]);
+				this.launchRocket(i);
 			this.step = ++this.step % this.timeline.length; // loop
 		}
 		++this.tick;
@@ -258,7 +427,7 @@
 			particle = this.particles[i];
 			particle.update();
 			if ( particle.enabled )
-				this.draw3Din2D(particle);
+				this.draw3Din2D(particle, i);
 		}
 	};
 
@@ -376,4 +545,20 @@
 			$.error( 'Method ' +  method + ' does not exist on jQuery.fireworks' );
 		}
 	};
+
+	if ( typeof Array.prototype.random == "undefined" ) {
+		Array.prototype.random = function(fun) {
+			var rand, idx;
+			if ( this.length == 0 )
+				return;
+			if ( this.length == 1 )
+				return this[0];
+			if ( typeof fun == "function" )
+				rand = fun.apply();
+			else
+				rand = Math.random();
+			idx = Math.floor( rand * this.length );
+			return this[ idx ];
+		};
+	}
 })( jQuery );
